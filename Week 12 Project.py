5af361c38484a380d46eab78f4d7dd479c88c4d1
theCaptN21 <khenr89@wgu.edu>
2022-12-01 17:29:30 +0000

    Updates to Week 12 project

---
+++ Week 12 Project.py
@@ -0,0 +1,28 @@
+#Create your empty list of AWS services
+myaws_services= []
+
+#Populate your list of AWS Services
+myaws_services.append('AWS Lambda')
+myaws_services.append('AWS Code Pipeline')
+myaws_services.append('Amazon Inspector')
+myaws_services.append('AWS CloudHSM')
+myaws_services.append('AWS Shield')
+myaws_services.append('Amazon Cognito')
+myaws_services.append('Amazon ElastiCache')
+myaws_services.append('AWS Security Hub')
+myaws_services.append('AWS Fargate')
+myaws_services.append('Amazon EventBridge')
+
+#Print the list of AWS Services and its length
+print("10 Must include AWS and Amazon Services", len(myaws_services))
+
+#Remove two specific AWS Services from the list by name or by index
+del myaws_services[0]
+del myaws_services[-1]
+
+#Print the new list and its new legth length
+print(myaws_services)
+print("8 Must include AWS and Amazon Services", len(myaws_services))
+
+
+
