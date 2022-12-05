6fae9a0d4269677b70d3f8dd4c05a7478124d705
theCaptN21 <khenr89@wgu.edu>
2022-12-05 17:28:21 +0000

    Changes to generator script

---
+++ Random_Name_Generator.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3.7
+
+#EC2 Random Name Generator
+
+#Resources: 
+#https://docs.python.org/3/library/random.html 
+#https://pynative.com/python-generate-random-string/
+
+import random
+import string
+import time
+
+
+#Random name generator function
+def string_generator(size=6, string=string.ascii_letters + string.digits):
+    return ''.join(random.choice(string) for _ in range(size))
+
+department = input("Are you a member of Accounting, FinOps, or Marketing? If so, enter the dept name.\n").upper()  
+    
+for _ in department:
+    
+    if department == "Accounting" or department.lower() == "accounting":
+        print("Verification in process, one moment please...")
+        time.sleep(5)
+        break
+    
+    elif department == "FinOps" or department.lower() == "finops":
+        print("Verification in process, one moment please...")
+        time.sleep(5)
+        break
+    
+    elif department == "Marketing" or department.lower() == "marketing":
+        print("Verification in process, one moment please...")
+        time.sleep(5)
+        break
+    
+    else:
+        print("Error ❌: Department not verified. Enter the correct Department to gain access to this Name Generator.")
+        raise TimeOut
+        time.sleep(5)  
+
+number = int(input("Input the number of EC2 instances that require names: "))
+    
+if number < 0:
+    print("Please enter at least one number: ")
+elif number > 0:
+    print("Input accepted")
+    
+#Results should be printed
+print("\n...Results are being generated...\n")
+time.sleep(5)
+print("Here are your new EC2 Instance Names!!")
+
+for _ in range(1, number + 1):
+    EC2_name = department
+    unique_ID_name = EC2_name + "-" + string_generator()
+    print("Your New EC2 Instance Name : ", unique_ID_name)
+    
+print("Please visit Katoria's name generator again!! 😊")
+
+
+
