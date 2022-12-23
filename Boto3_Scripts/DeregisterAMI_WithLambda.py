eb04e7cb68d0bb010b3c5876ada54631cfb2ccdc
theCaptN21 <khenr89@wgu.edu>
2022-12-23 16:59:20 +0000

    Updates to the script

---
+++ Boto3_Scripts/DeregisterAMI_WithLambda.py
@@ -0,0 +1,40 @@
+#This is a script to deregister an AMI using Lambda
+import datetime
+from dateutil.parser import parse
+
+import boto3
+
+def days_old(date):
+    parsed = parse(date).replace(tzinfo=None)
+    diff = datetime.datetime.now() - parsed
+    return diff.days
+    
+
+def lambda_handler(event, context):
+    
+    #Get a list of regions
+    ec2_client = boto3.client('ec2')
+    regions = [region['RegionName']
+               for region in ec2_client.describe_regions()['Regions']]
+               
+    for region in regions:
+        ec2 = boto3.clent('ec2', region_name=region)
+        print("Region:", region)
+        
+        amis = ec2.describe_images(Owners=['self'])['Images']
+        
+        for ami in amis:
+            creation_date = ami['CreationDate']
+            age_days = days_old(creation_date)
+            image_id = ami['ImageId']
+            print('ImageId: {}, CreationDate: {} ({} days old)'.format(
+                image_id, creation_date, age_days))
+                
+            if age_days >= 2:
+                print('Deleting ImageId:', image_id)
+                
+                #Deregister the AMI
+                ec2.deregister_image(ImageId=image_id)
+                
+                
+
